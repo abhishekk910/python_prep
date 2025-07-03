5e0038528aa62d0dd164e268aefde5de2467ee11
abhishekk910 <abhikrishna.mbl@gmail.com>
2025-07-03 17:28:27 +0530

    removed

--- tuples/file1.py
+++
@@ -1,9 +0,0 @@
-tuple1 = (1,2,3)
-tuple2 = (4,5,6)
-
-tuple3 = ()
-
-for  i in range(len(tuple1)):
-    tuple3 += (tuple1[i] + tuple2[i],)
-
-print(tuple3)  # Output: (5, 7, 9)  
--- tuples/file2.py
+++
@@ -1,7 +0,0 @@
-tuple3 = (1,2,3)
-tuple4 = (4,5,7)
-for i in range(len(tuple3)):
-    print(tuple4[i])
-
-
-
