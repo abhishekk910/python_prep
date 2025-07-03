99308d703f3250d496cb955fb53cbc0983f7b669
Abhishek Krishna Murthy <amurthy@AD.MSYSTECHNOLOGIES.COM>
2025-07-03 17:16:20 +0530

    added

---
+++ tuples/file3.py
@@ -0,0 +1,18 @@
+#Tuple Problem: Extract Even Numbers from Tuple
+"""
+Write a Python program to:
+
+    Extract all even numbers from the tuple.
+
+    Store them in a new tuple.
+
+    Print the new tuple.
+"""
+numbers = (10, 15, 22, 33, 40, 55, 60)
+
+even_tuples = tuple()
+
+for number in numbers:
+    if number % 2 == 0:
+        even_tuples += (number,)
+print(even_tuples)  
